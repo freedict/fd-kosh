4fb41215ed5f73debe0f849aeda69455c4138e57
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-03-31 21:58:14 +0200

    check if target dir is empty

--- update_freedict.sh
+++ update_freedict.sh
@@ -2,11 +2,16 @@
 
 #$1 abs_path_to_fd_files e.g. '/home/me/repositories/freedict/fd-data'
 
-if [ ! -d "${1}" ]; then
-  # if the path does not exist, create it and download all dicts from freedict.org
+#if dir exists
+if [ -d "${1}" ]; then
+  #if empty
+  if [ -z "$(ls -A ${1})" ]; then
+    python update_freedict.py ${1} --init
+  else
+    python update_freedict.py ${1}
+  fi
+else
+  #if dir does not exists
   mkdir -p ${1}
   python update_freedict.py ${1} --init
-else
-  # update the files
-  python update_freedict.py ${1}
 fi
