14a8f0e382365704b56670c7f9548b3ca96b2b5b
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-04-09 14:05:16 +0200

    var refactoring

--- update_freedict.py
+++ update_freedict.py
@@ -62,21 +62,21 @@ def get_freedict_list_from_upstream(freedict_url):
     return j
 
 
-def init_download(freedict_url, path_to_local_files, logger):
+def init_download(freedict_url, path_to_xml_files, logger):
     parsed_freedict_file = parse_freedict_json(get_freedict_list_from_upstream(freedict_url))
     # create dir if does not exists
-    os.makedirs(path_to_local_files, exist_ok=True)
+    os.makedirs(path_to_xml_files, exist_ok=True)
     for dict_id, props in parsed_freedict_file.items():
         # get tar from freedict.org
-        get_dict_from_upstream(path_to_local_files, parsed_freedict_file, dict_id)
+        get_dict_from_upstream(path_to_xml_files, parsed_freedict_file, dict_id)
         print(dict_id, 'downloaded and decompressed')
         # save a local copy of fredict-json
-    with open('{}/freedict-database.json'.format(path_to_local_files), 'w') as f:
+    with open('{}/freedict-database.json'.format(path_to_xml_files), 'w') as f:
         json.dump(get_freedict_list_from_upstream(freedict_url), f, indent=4)
-        logger.info('Local copy of freedict downloaded at {}'.format(path_to_local_files))
+        logger.info('Local copy of freedict downloaded at {}'.format(path_to_xml_files))
 
 
-def compare_and_download_new_dicts(local_freedict, freedict_url, path_to_local_files, logger):
+def compare_and_download_new_dicts(local_freedict, freedict_url, path_to_xml_files, logger):
     upstream_freedict = parse_freedict_json(get_freedict_list_from_upstream(freedict_url))
     modified = False
     # check intersection of dict_ids
@@ -85,22 +85,22 @@ def compare_and_download_new_dicts(local_freedict, freedict_url, path_to_local_f
         upstream_cheksum = upstream_freedict.get(dict_id).get('checksum')
         if local_checksum != upstream_cheksum:
             modified = True
-            get_dict_from_upstream(path_to_local_files, upstream_freedict, dict_id)
+            get_dict_from_upstream(path_to_xml_files, upstream_freedict, dict_id)
             logger.info('{} has been updated from upstream'.format(dict_id))
 
     ## check diff. we assume that new dictionaries are included and existing dictionaries are not removed from freedict.org
     for dict_id in upstream_freedict.keys() - local_freedict.keys():
         modified = True
-        get_dict_from_upstream(path_to_local_files, upstream_freedict, dict_id)
+        get_dict_from_upstream(path_to_xml_files, upstream_freedict, dict_id)
         logger.info('{} has been downloaded from upstream'.format(dict_id))
         # generate kosh_files for this dictionary
-        generate_kosh_files.generate_dot_kosh(path_to_local_files, dict_id)
-        generate_kosh_files.generate_mapping(path_to_local_files, dict_id)
+        generate_kosh_files.generate_dot_kosh(path_to_xml_files, dict_id)
+        generate_kosh_files.generate_mapping(path_to_xml_files, dict_id)
         logger.info('kosh files generated for {}'.format(dict_id))
 
     if modified:
         # replace local copy of fredict-json
-        with open('{}/freedict-database.json'.format(path_to_local_files), 'w') as f:
+        with open('{}/freedict-database.json'.format(path_to_xml_files), 'w') as f:
             json.dump(get_freedict_list_from_upstream(freedict_url), f, indent=4)
         logger.info('new freedict-database.json saved')
 
@@ -110,19 +110,19 @@ def main(args):
     logger = get_logger()
     freedict_url = 'https://freedict.org/freedict-database.json'
     if args.init:
-        init_download(freedict_url, args.path_to_freedict, logger)
-        generate_kosh_files.generate_all_kosh_files(args.path_to_freedict)
+        init_download(freedict_url, args.path_to_xml_files, logger)
+        generate_kosh_files.generate_all_kosh_files(args.path_to_xml_files)
         logger.info('kosh files generated for all dicts')
     else:
-        path_to_json_file = '{}/freedict-database.json'.format(args.path_to_freedict)
+        path_to_json_file = '{}/freedict-database.json'.format(args.path_to_xml_files)
         local_freedict = get_local_freedict(path_to_json_file)
         compare_and_download_new_dicts(local_freedict, freedict_url,
-                                       args.path_to_freedict, logger)
+                                       args.path_to_xml_files, logger)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('path_to_freedict', help='path to freedict files')
+    parser.add_argument('path_to_xml_files', help='path to freedict xml files')
     parser.add_argument("--init", help="initial download from freedict.org",
                         action="store_true")
     args = parser.parse_args()
